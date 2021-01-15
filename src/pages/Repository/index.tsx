5c1727d658ca57aa3a503b7b3c5b9624165d1428
Bruno Rocha <brunorocha-brs@outlook.com>
2021-01-15 09:44:14 -0300

    Added repository components

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
-import { FiChevronLeft } from 'react-icons/fi';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Header } from './styles';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -22,6 +22,41 @@ const Repository: React.FC = () => {
           Voltar
         </Link>
       </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="Rocketseat" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>descrição do repo</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="asdasd">
+          <div>
+            <strong>asdasdasd</strong>
+            <p>asdasdasdas</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
     </>
   );
 };
